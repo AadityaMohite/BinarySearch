ff368cdca18ba300ebb3bef09950e49db4ddf1aa
Aaditya_coder❤️ <mohiteaadi10@gmail.com>
2025-12-02 23:18:08 +0530

    Push after pull

--- src/FirstandLastPosition.java
+++ src/FirstandLastPosition.java
@@ -1,70 +1,60 @@
 import java.util.Scanner;
 
 public class FirstandLastPosition {
-       public static void main(String[] args){
-           Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-           System.out.println("Enter the size of the array");
-           int n = sc.nextInt();
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
 
-           int [] arr = new int[n];
+        int [] arr = new int[n];
 
-           System.out.println("Enter the elements of an array");
-           for (int i = 0; i < arr.length; i++) {
-               arr[i] = sc.nextInt();
-           }
+        System.out.println("Enter the elements of an array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
 
-           System.out.println("Enter the target element");
-           int target = sc.nextInt();
+        System.out.println("Enter the target element");
+        int target = sc.nextInt();
 
-           searchRange(arr,target);
+        int[] ans = searchRange(arr, target);   // FIXED
+        System.out.println("First position: " + ans[0]);
+        System.out.println("Last position: " + ans[1]);
+    }
 
-       }
     static int[] searchRange(int[] nums, int target) {
-         int[] ans = {-1,-1};
+        int[] ans = {-1,-1};
 
-         int start = search(nums, target, true);
-         int end = search(nums,target, false);
+        int start = search(nums, target, true);
+        int end = search(nums,target, false);
 
-         ans[0] = start;
-         ans[1] = end;
+        ans[0] = start;
+        ans[1] = end;
 
-         return ans;
-
+        return ans;
     }
 
     static int search(int[] nums, int target, boolean firstIndexposition){
         int ans = -1;
         int start = 0;
-        int end =  nums.length-1;
+        int end = nums.length - 1;
 
-
-
-
-        while(start<=end) {
-
+        while(start <= end) {
             int mid = (start + end) / 2;
 
-            if(nums[mid]<target){
-                start = mid+1;
-            } else if (nums[mid]>target) {
-                end = mid-1;
-            }else {
+            if(nums[mid] < target){
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
                 ans = mid;
                 if (firstIndexposition){
-                    end = mid-1;
-                }else{
-                    start = mid+1;
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
             }
-
         }
-
-
-
-   return  ans;
-
-
+        return ans;
     }
-
 }
